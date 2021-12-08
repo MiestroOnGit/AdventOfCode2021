fc65532f891618b796bdf664becf88ea5237ffa6
Miestro <miestro@miestrogames.com>
2021-12-07 22:09:38 -0500

    Day3 part two

--- DayThree/GammaSolver.cs
+++ DayThree/GammaSolver.cs
@@ -10,7 +10,6 @@ namespace DayThree
         private int _shiftSize;
         private int _bitWidth;
         private ulong bitMask;
-        private int[] _setPosCount;
         private ulong _gamma;
         private ulong _epsolon;
         private int _entrySize;
@@ -20,7 +19,6 @@ namespace DayThree
         {
             _bitWidth = bitWidth;
             _shiftSize = bitWidth - 1;
-            _setPosCount = new int[bitWidth];
             _epsolon = 0;
             _gamma = 0;
             _entrySize = input.Count();
@@ -31,34 +29,101 @@ namespace DayThree
                 bitMask |= (uint) (1 << i);
             }
         }
-
-        public ulong solve()
+        
+        public GammaSolver(ulong[] input, int bitWidth)
         {
-            foreach (var value in inputValues)
-            {
-                for (int i = 0; i < _bitWidth; i++)
-                {
-                    ulong pos = (ulong) (1L << (_shiftSize - i));
-                    bool set = (value & pos) != 0; 
-                    _setPosCount[i] += Convert.ToInt32(set);
-                }
-            }
-
+            _bitWidth = bitWidth;
+            _shiftSize = bitWidth - 1;
+            _epsolon = 0;
             _gamma = 0;
+            _entrySize = input.Count();
+            inputValues = input;
 
-            for (int i = 0; i < _setPosCount.Length; i++)
+            for (int i = 0; i < _bitWidth; i++)
             {
-                if (_setPosCount[i] > (_entrySize/2))
-                {
-                    //Mostly 1s
-                    _gamma |= (uint) (1 << (_shiftSize - i));
-                }
+                bitMask |= (uint) (1 << i);
             }
+        }
 
+        public ulong SolveGammaEpsolon()
+        {
+            _gamma = CalculateGammaFor(inputValues);
+            
             _epsolon = (bitMask & (~_gamma));
             
             return _epsolon * _gamma;
-        } 
+        }
 
+        public ulong CalculateLifeSupport()
+        {
+            ulong[] oxygenWorkingArray = new ulong[inputValues.Count()]; 
+            ulong[] carbonWorkingArray = new ulong[inputValues.Count()]; 
+            Array.Copy(inputValues, oxygenWorkingArray, inputValues.Length);
+            Array.Copy(inputValues, carbonWorkingArray, inputValues.Length);
+
+            ulong oxygenVal = 0;
+            ulong carbonDioxideVal = 0;
+            
+            for (int i = _shiftSize; i >= 0; i--)
+            {
+                if (oxygenWorkingArray.Length > 1)
+                {
+                    ulong[] oxygenPositionSet = CalculateBitSetPositionCount(oxygenWorkingArray);
+                    bool oxygenIsOne = oxygenPositionSet[_shiftSize - i] >= (double) oxygenWorkingArray.Length / 2;
+                    oxygenWorkingArray = FilterWhereBitIs(oxygenIsOne, i, oxygenWorkingArray);
+                }
+                if (carbonWorkingArray.Length > 1)
+                {
+                    ulong[] carbonSetPositionCount = CalculateBitSetPositionCount(carbonWorkingArray);
+                    bool carbonIsOne = carbonSetPositionCount[_shiftSize - i] < (double) carbonWorkingArray.Length / 2;
+                    carbonWorkingArray = FilterWhereBitIs(carbonIsOne, i, carbonWorkingArray);
+                }
+            }
+
+            return oxygenWorkingArray[0] * carbonWorkingArray[0];
+        }
+
+        private ulong CalculateGammaFor(ulong[] startArray)
+        {
+            ulong calculatedGamma = 0;
+            var setPosCount = CalculateBitSetPositionCount(startArray);
+
+            for (int i = 0; i < setPosCount.Length; i++)
+            {
+                if (setPosCount[i] > (ulong) (_entrySize / 2))
+                {
+                    //Mostly 1s
+                    calculatedGamma |= (uint) (1 << (_shiftSize - i));
+                }
+            }
+
+            return calculatedGamma;
+        }
+
+        private ulong[] CalculateBitSetPositionCount(ulong[] startArray)
+        {
+            ulong[] setPosCount = new ulong[_bitWidth];
+            foreach (var value in startArray)
+            {
+                for (int i = 0; i < _bitWidth; i++)
+                {
+                    setPosCount[i] += Convert.ToUInt64(CheckAtPosition(value, _shiftSize - i, true));
+                }
+            }
+
+            return setPosCount;
+        }
+
+        private ulong[] FilterWhereBitIs(bool set, int position, IEnumerable<ulong> inputArray)
+        {
+            return inputArray.Where(v => CheckAtPosition(v, position, set)).ToArray();
+        }
+
+        private bool CheckAtPosition(ulong value, int position, bool isSet)
+        {
+            ulong pos = (ulong) (1L << position);
+            bool set = isSet ? (value & pos) != 0 : (value & pos) == 0;
+            return set;
+        }
     }
 }
--- DayThree/Program.cs
+++ DayThree/Program.cs
@@ -11,6 +11,8 @@ IEnumerable<string> input = FileReader.ReadFileNameToCollection<string>(@"input.
 //Assuming all the binary strings are the same length
 GammaSolver solver = new(input, input.ElementAt(0).Length);
 
-ulong solution = solver.solve();
-
+ulong solution = solver.SolveGammaEpsolon();
 Console.WriteLine(solution);
+
+ulong solutionTwo = solver.CalculateLifeSupport();
+Console.WriteLine(solutionTwo);
